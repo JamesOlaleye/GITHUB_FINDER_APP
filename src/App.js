52c9b7c28779212912be28ab2146c29df6158982
James Olaleye <olanipekunjamesolaleye@gmail.com>
2023-02-12 13:15:09 +0100

    Updated corrections

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import User from './components/users/User';
 import Alert from './components/layout/Alert';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
-import NotFound from './components/pages/NotFound';
+//import NotFound from './components/pages/NotFound';
 
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
@@ -21,12 +21,11 @@ const App = () => {
             <Navbar />
             <div className='container'>
               <Alert />
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-                <Route exact path='/user/:login' component={User} />
-                <Route component={NotFound} />
-              </Switch>
+              <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route exact path='/about' element={<About />} />
+                <Route exact path='/user/:login' element={<User />} />
+              </Routes>
             </div>
           </div>
         </Router>
